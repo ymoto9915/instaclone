9017871c841e3ffe96a22f5fed9b1954b7d383a0
mikan <mikan927ty@gmail.com>
2020-01-18 17:07:24 +0900

    index, new, create追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,13 +1,27 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = Picture.all
   end
 
   def show
   end
 
   def new
+    @picture = Picture.new
   end
 
   def edit
+  end
+
+  def create
+    picture = Picture.new(picture_params)
+    picture.save!
+    redirect_to pictures_url, notice: "投稿完了"
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:image)
   end
 end
