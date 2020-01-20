bc954e60fd25c2f2fc47898847989a813da62abc
mikan <mikan927ty@gmail.com>
2020-01-20 15:21:00 +0900

    写真編集機能追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -12,6 +12,13 @@ class PicturesController < ApplicationController
   end
 
   def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    picture = Picture.find(params[:id])
+    picture.update!(picture_params)
+    redirect_to pictures_url, notice: "編集しました"
   end
 
   def create
